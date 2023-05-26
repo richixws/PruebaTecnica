8afc2b3484f43be7f1a11f10a1fdf280b8efc8df
Richard Wong <rwongsol@hotmail.com>
2023-05-25 19:12:52 -0500

    agregacion de spring security a las apis

---
+++ prueba-empresa/src/main/java/com/prueba/com/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.prueba.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http.csrf().disable()
+      .authorizeRequests().antMatchers("/**").permitAll()
+        .anyRequest().authenticated().and().httpBasic();
+
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+
+    UserDetails user= User.builder().username("empresa").password(passwordEncoder().encode("12345")).roles("USER").build();
+    UserDetails admin= User.builder().username("admin").password(passwordEncoder().encode("12345")).roles("ADMIN").build();
+    return new InMemoryUserDetailsManager(user,admin);
+
+  }
+}
--- prueba-empresa/src/main/java/com/prueba/com/controller/EmpresaController.java
+++ prueba-empresa/src/main/java/com/prueba/com/controller/EmpresaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,13 +18,16 @@ public class EmpresaController {
     @Autowired
     private EmpresaService empleadoService;
 
+
     @PostMapping("/empresas")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Empresa> agregarEmpleado(@RequestBody Empresa empleado){
 
          return new ResponseEntity<>(empleadoService.createEmpresa(empleado), HttpStatus.CREATED);
     }
 
     @GetMapping("/empresas")
+    @PreAuthorize("hasRole('USER')")
   public ResponseEntity<List<Empresa>> listarEmpleados(){
 
     return ResponseEntity.ok().body(empleadoService.listAllEmpresa());
@@ -31,6 +35,7 @@ public class EmpresaController {
   }
 
   @GetMapping("/empresas/registro")
+  @PreAuthorize("hasRole('USER')")
   public ResponseEntity<List<Empresa>> listarEmpleados(@RequestParam("limit") int limit){
 
     return ResponseEntity.ok().body(empleadoService.obtenerUltimosTresRegistros(limit));
